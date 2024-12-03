8aff3b0bdea5880394d1a273ec8989bc1cfc9b24
Essential1s <essentialis.ford@gmail.com>
2024-12-03 12:35:35 +0700

    пример работы с пакетами

---
+++ bookpack/pr010.java
@@ -0,0 +1,30 @@
+package bookpack;
+class Book {
+	private String title;
+	private String author;
+	private int pubDate;
+
+	Book(String t, String a, int d) {
+		title = t;
+		author = a;
+		pubDate = d;
+	}
+	void show() {
+		System.out.println(title);
+		System.out.println(author);
+		System.out.println(pubDate);
+		System.out.println();
+	}
+}
+
+class pr010 {
+	public static void main(String[] args) {
+		Book[] books = new Book[4];
+		books[0] = new Book("illiad", "gomer", 1941);
+		books[1] = new Book("Naedine s soboi","mark aurelius", 1950);
+		books[2] = new Book("Sobor paris godmother", "gugo", 1790);
+		books[3] = new Book("Crime and punishment", "Dostaevsky", 1950);
+		for(int i=0; i< books.length; i++)
+			books[i].show();
+	}
+}
