031a23f8674f7556bae9c76837951f8b31c0a693
Essential1s <essentialis.ford@gmail.com>
2024-12-10 11:38:12 +0700

    Добавили в ByTwos метод получения предыдущего значния

--- SeriesDemo.java
+++ SeriesDemo.java
@@ -3,14 +3,18 @@ class SeriesDemo {
 		ByTwos ob = new ByTwos();
 		for(int i = 0; i<5; i++)
 			System.out.println("Next val in sequence " + ob.getNext());
+		System.out.println("last previous value: " + ob.getPrevious());
+
 		System.out.println("\nReset to start val");
 		ob.reset();
 		for(int i = 0; i<5; i++)
 			System.out.println("Next val in sequence " + ob.getNext());
+		System.out.println("last previous value: " + ob.getPrevious());
+
                 System.out.println("\nSet new start val");
 		ob.setStart(100);
 		for(int i = 0; i<5; i++)
                         System.out.println("Next val in sequence " + ob.getNext());
-
+		System.out.println("last previous value: " + ob.getPrevious());
 	}
 }
